67761d372b93f0e20fac3597821964d73f869775
TKeesh <tkis.cro@gmail.com>
2017-05-19 17:39:58 +0200

    forward done, requirements

--- forward_resnet.py
+++ forward_resnet.py
@@ -9,12 +9,12 @@ import argparse
 
 from embeddings_processing_config import cfg
 
-def resnet(dataDir):
+
+
+def resnet(dataDir, layers):
     from convert import print_prob, load_image, checkpoint_fn, meta_fn
     import tensorflow as tf
-    import cv2
-
-    layers = 101
+    import cv2   
 
     sess = tf.Session()
 
@@ -73,19 +73,32 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser("Generates embeddings from network.")
 
-    parser.add_argument("dataDir", type=str, help="folder with dataset images", nargs=1)
-    parser.add_argument("--tb", action="store_true", help="genenrates tensorboard")
+    parser.add_argument("-dataDir", type=str, help="folder with dataset images (default: './data/')", nargs=1)
+    parser.add_argument("-m", type=str, help="choose ResNet model (50, 101 (default), 152)", nargs=1)
+    parser.add_argument("--tb", action="store_true", help="genenrates tensorboard ('./tensorboard/test_data')")
 
     args = parser.parse_args()  
 
-    dataDir = args.dataDir[0]
-    sess, images_list, image_names, EMB1, EMB2 = resnet(dataDir)
+    if args.m:
+        layers = args.m[0]
+    else:
+        layers = 101
+
+    if args.dataDir is None:
+        dataDir = './data'
+    else:
+        dataDir = args.dataDir[0]
     
+    sess, images_list, image_names, EMB1, EMB2 = resnet(dataDir, layers)
+    
+    if not os.path.exists('./output/'):
+        os.makedirs('./output/')
+
     print("Saving embeddings...")
     im_names = np.asarray(image_names)
     LOG_DIR_name = os.path.split(dataDir)
-    np.save(os.path.join(LOG_DIR_name[0], 'image_names_' + LOG_DIR_name[1]), im_names)
-    np.save(os.path.join(LOG_DIR_name[0], 'embeddings_' + LOG_DIR_name[1]), EMB2)
+    np.save(os.path.join('./output/', 'image_names_' + LOG_DIR_name[1]), im_names)
+    np.save(os.path.join('./output/', 'embeddings_' + LOG_DIR_name[1]), EMB2)
     print("Done.")
 
     if args.tb:
